60738228bb6f50d521fcd701a08d168a93036bd1
Andrey Golikov <andrey@golikov.net>
2020-12-07 18:50:10 +0300

    Move unnecessary classes from main module

--- partial-autowire-test-support/build.gradle.kts
+++ partial-autowire-test-support/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies {
 
     implementation("org.springframework", "spring-test", springVersion)
     implementation("org.springframework", "spring-context", springVersion)
+    implementation("org.springframework", "spring-web", springVersion)
+    implementation("org.springframework.boot", "spring-boot", "2.3.1.RELEASE")
 
     testImplementation("junit", "junit", "4.12")
     testImplementation("org.assertj:assertj-core:3.15.0")
--- partial-autowire/build.gradle.kts
+++ partial-autowire/build.gradle.kts
@@ -4,6 +4,4 @@ dependencies {
     implementation(project(":util"))
 
     implementation("org.springframework", "spring-context", springVersion)
-    implementation("org.springframework", "spring-web", springVersion)
-    implementation("org.springframework.boot", "spring-boot", "2.3.1.RELEASE")
 }
